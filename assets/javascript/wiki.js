deb5e7c1fd0fb9594523eaf23e7f70336b8a2ffc
Dan Smith <daniel.smith8795@gmail.com>
2019-07-21 13:31:55 -0500

    added in adjustable search term for post quiz results

--- assets/javascript/wiki.js
+++ assets/javascript/wiki.js
@@ -4,9 +4,9 @@ var snippet = $("#snippet");
 var pic = $("#plantpic");
 var care = $("#caretips");
 var wikilink = $("#wikilink");
-var searchterm;
+var searchterm = 'Saintpaulia';
 //query URL will need to be adjusted later once quiz function is up and running
-var queryURL = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search='Saintpaulia'&limit=1&format=json";
+var queryURL = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=" + searchterm + "&limit=1&format=json";
 $.ajax({
     url: queryURL,
     method: "GET"
